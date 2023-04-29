76ed1c1aaea9274cfbae4c2da2c4b37a57d6dba8
unknown <rajamuneeb2018@gmail.com>
2023-04-29 15:16:56 +0500

    Face Detection Task

--- lib/main.dart
+++ lib/main.dart
@@ -67,33 +67,3 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-
-
-// import 'package:camera/camera.dart';
-// import 'package:face_detection/face_detection.dart';
-// import 'package:flutter/material.dart';
-// import 'package:cupertino_icons/cupertino_icons.dart';
-
-// List<CameraDescription>? cameras;
-
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   cameras = await availableCameras();
-
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Flutter Demo',
-//       theme: ThemeData(),
-//       home: FaceDetection(),
-//     );
-//   }
-// }
